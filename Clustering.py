4cd294432f405ee7c2a43993b6457aa2593204d9
Akos Furton <Akos@dyn1188-215.wlan.ic.ac.uk>
2016-10-12 11:25:15 +0100

    Clustering Algorithm in Progress

---
+++ Clustering.py
@@ -0,0 +1,125 @@
+class Digraph():
+    #edges is a dict mapping each node to a dictionary of edges starting from it
+    def __init__(self,filename = None):
+        self.edges = {}
+        self.numEdges = 0
+    
+    def addNode(self,node):
+        # add a vertex to graph (based on key value)       
+        self.edges[node] = set()
+
+    def addEdge(self,src,dest,weight):
+        # add the (v,w) edge, making sure the two nodes exist
+        if not self.hasNode(src): 
+            self.addNode(src)
+            self.edges[src] = {}
+        if not self.hasNode(dest): 
+            self.addNode(dest)
+            self.edges[dest] = {}
+        if not self.hasEdge(src, dest):
+            self.numEdges += 1
+            self.edges[src][dest] = weight
+  
+    def childrenOf(self, v):
+        # Returns a node's children
+        return self.edges[v].items()
+        
+    def hasNode(self, v):
+        return v in self.edges
+        
+    def hasEdge(self, v, w):
+        return w in self.edges[v]
+
+    def listEdges(self):
+        ll = []
+        for src,values in self.edges.items():
+            for dest,weight in values.items():
+                ll.append([src,dest,weight])
+        return ll
+    
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest,weight in self.edges[src].items():
+                result = result + src + '->'\
+                         + dest + ', length ' + str(weight) + '\n'
+        return result[:-1] # omit final newline
+
+class Graph(Digraph):
+    """ Undirected graph: two one-way edges for every edge
+        """
+    def addEdge(self, src, dest,weight):
+        Digraph.addEdge(self, src, dest,weight)
+        Digraph.addEdge(self, dest, src,weight)
+
+input = [('a','b',.8),('a','c',.63),('c','d',.95),('b','d',.32),('b','c',.68),('a','d',.05)]
+
+def mergeSort(array):
+	if len(array) > 1:
+		mid = len(array) //2
+		left = array[:mid]
+		right = array [mid:]
+		mergeSort(left)
+		mergeSort(right)
+
+		i = 0
+		j = 0
+		k = 0
+		while i < len(left) and j < len(right):
+			if left[i][2] > right[j][2]:
+				array[k] = left[i]
+				i = i + 1
+			else:
+				array[k] = right[j]
+				j = j + 1
+			k = k+1
+		while i < len(left):
+			array[k] = left[i]
+			i += 1
+			k += 1
+		while j < len(right):
+			array[k] = right[j]
+			j += 1
+			k += 1
+	return(array)
+sortedWeights = mergeSort(input)
+print(sortedWeights)
+
+graph = Digraph()
+for x in input:
+	graph.addEdge(x[0],x[1],x[2])
+#print(graph)
+
+nodePointers = {src:src for src in graph.edges}
+
+print (nodePointers)
+
+for k in sortedWeights:
+
+	#only connect sink nodes
+	nodePointers[k[0]] = k[1]
+	print(nodePointers)
+
+#how to recover sets
+
+#print(nodePointers)
+
+'''
+listSets = []
+for src in graph.edges:
+	listSets.append(set(src))
+#print(listSets)
+
+
+
+for k in sortedWeights:
+	source = k[0]
+	destination = k[1]
+	#print(source,destination)
+	for element in listSets:
+		if source in element:
+			element.pop()
+		#print(listSets)
+
+'''
+
--- DSA_project.py
+++ DSA_project.py
@@ -17,12 +17,80 @@ percent_change = priceData.pct_change()
 #print(percent_change)
 
 correlations = percent_change.corr()
-#print(correlations)
+
+correlations = correlations.where(np.triu(np.ones(correlations.shape)).astype(np.bool))
+correlations = correlations.stack().reset_index()
+correlations.columns = ['Company1', 'Company2', 'Correlation']
+
+correlation_tuples = [tuple(x) for x in correlations.values]
+
+# Sort edges in decreasing correlation
+
+def mergeSort(array):
+	if len(array) > 1:
+		mid = len(array) //2
+		left = array[:mid]
+		right = array [mid:]
+		mergeSort(left)
+		mergeSort(right)
+
+		i = 0
+		j = 0
+		k = 0
+		while i < len(left) and j < len(right):
+			if left[i][2] > right[j][2]:
+				array[k] = left[i]
+				i = i + 1
+			else:
+				array[k] = right[j]
+				j = j + 1
+			k = k+1
+		while i < len(left):
+			array[k] = left[i]
+			i += 1
+			k += 1
+		while j < len(right):
+			array[k] = right[j]
+			j += 1
+			k += 1
+	return(array)
+
+print(mergeSort(correlation_tuples))
+
+class Node:
+	def __init__(self, group, inCluster):
+		self.group = -1
+		self.inCluster = False
+
+	def getGroup(self):
+		return self.group
+
+	def setGroup(self,group):
+		self.group = group
+
+	def addToCluster(self):
+		self.inCluster = True
+
+	def checkCluster(self):
+		return self.inCluster
+
+#for x in correlation_tuples:
+
+
+
+#Implement Kruskal's clustering algorithm
+#def clustering(array, k):
+
+
+
+
+
+
 
 #print(correlations.get_value('AAPL','MMM'))
 
-aapl_correlations = correlations.loc[:,'AAPL']
-print(aapl_correlations.order(ascending = False))#.iloc[-1])
+#aapl_correlations = correlations.loc[:,'AAPL']
+#print(aapl_correlations.order(ascending = False))#.iloc[-1])
 
 #print(aapl_correlations.iloc[0])
 
